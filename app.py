556225a970b2c9b6b2e427361c4e4ca51c78ef88
ericjonesdev <>
2023-10-28 16:19:49 +0000

    update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_cells_visualizer import page_cells_visualizer_body
-from app_pages.page_malaria_detector import page_malaria_detector_body
+from app_pages.page_cherry_mildew_detector import page_cherry_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
