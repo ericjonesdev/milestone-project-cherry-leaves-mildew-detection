00715fbd21aaad5a41166badb82154989afeb880
ericjonesdev <>
2023-10-30 17:44:53 +0000

    update predictive analytics image resize

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -41,7 +41,7 @@ def resize_input_image(img, version):
     Reshape image to average image size
     """
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize((image_shape[1], image_shape[0]), Image.Resampling.LANCZOS)
+    img_resized = img.resize(int(image_shape[1], image_shape[0]), Image.Resampling.LANCZOS)
     my_image = np.expand_dims(img_resized, axis=0)/255
 
     return my_image
