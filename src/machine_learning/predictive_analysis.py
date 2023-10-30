f756f785774fcaa407eed3432905b1ddd3d094c6
ericjonesdev <>
2023-10-30 19:15:20 +0000

    edit predictive analytics

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -17,7 +17,7 @@ def plot_predictions_probabilities(pred_proba, pred_class):
 
     prob_per_class = pd.DataFrame(
         data=[0, 0],
-        index={'Parasitised': 0, 'Uninfected': 1}.keys(),
+        index={'mildew': 0, 'healthy': 1}.keys(),
         columns=['Probability']
     )
     prob_per_class.loc[pred_class] = pred_proba
@@ -52,17 +52,17 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"outputs/{version}/malaria_detector_model.h5")
+    model = load_model(f"outputs/{version}/cherry_mildew_detector_model.h5")
 
     pred_proba = model.predict(my_image)[0, 0]
 
-    target_map = {v: k for k, v in {'Parasitised': 0, 'Uninfected': 1}.items()}
+    target_map = {v: k for k, v in {'mildew': 0, 'healthy': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
 
     st.write(
-        f"The predictive analysis indicates the sample cell is "
+        f"The predictive analysis indicates the sample leaf is "
         f"**{pred_class.lower()}** with malaria.")
 
     return pred_proba, pred_class
