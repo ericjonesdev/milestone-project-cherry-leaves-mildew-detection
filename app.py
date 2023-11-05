3e7c9b0df87cf16f52a1e7d841e41903c4764114
ericjonesdev <>
2023-11-05 10:52:39 +0000

    update app.py to account for function name change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
-from app_pages.page_leaf_visualizer import page_cells_visualizer_body
+from app_pages.page_leaf_visualizer import page_leaf_visualizer_body
 from app_pages.page_cherry_mildew_detector import page_cherry_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
