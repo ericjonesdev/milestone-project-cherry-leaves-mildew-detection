fd3c54f0c52175158bc0806a56c170e8b724dd77
ericjonesdev <>
2023-11-05 10:39:08 +0000

    update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
-from app_pages.page_cells_visualizer import page_cells_visualizer_body
+from app_pages.page_leaf_visualizer import page_cells_visualizer_body
 from app_pages.page_cherry_mildew_detector import page_cherry_mildew_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
@@ -12,7 +12,7 @@ app = MultiPage(app_name="Cherry Leaf Mildew Detector")  # Create an instance of
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Cells Visualiser", page_cells_visualizer_body)
+app.add_page("Leaf Visualiser", page_cells_visualizer_body)
 app.add_page("Mildew Detection", page_cherry_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
